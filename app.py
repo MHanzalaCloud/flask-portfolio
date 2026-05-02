a919d8cf776e317d760e155d30bf992823bf6b87
MHanzalaCloud <hanzala.142sb@gmail.com>
2026-05-02 07:09:04 +0000

    connected flask to mysql database

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
 from flask import Flask
-from datetime import datetime
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'flaskuser'
+app.config['MYSQL_PASSWORD'] = 'StrongPass123!'
+app.config['MYSQL_DB'] = 'flaskapp'
+
+mysql = MySQL(app)
+
 @app.route('/')
 def home():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return f'<h1>Hello from Flask!</h1><p>Server time: {current_time}</p>'
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT * FROM users")
+    users = cursor.fetchall()
+    cursor.close()
+    output = '<h1>Users from Database</h1>'
+    for user in users:
+        output += f'<p>{user[1]} - {user[2]}</p>'
+    return output
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
